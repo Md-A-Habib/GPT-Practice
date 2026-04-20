730775806c8fa0e26549a4c19daacdeee8f5b61e
Md Ahsan Habib <habibahsan9503@gmail.com>
2026-04-21 02:34:34 +0600

    Implement palindrome check in P-02.cpp

--- P-02.cpp
+++ P-02.cpp
@@ -207,6 +207,31 @@ int main(){
     }
 }
 
+
+
+====>>>>>>>>>> Alternative
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    int i = 0;
+    int j = s.size() - 1;
+
+    while(i < j){
+        if(s[i] != s[j]){
+            cout << "NO" << endl;
+            return 0;
+        }
+        i++;
+        j--;
+    }
+
+    cout << "YES" << endl;
+}
 */
 
 
