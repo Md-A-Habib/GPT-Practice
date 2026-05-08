cc9c283d2747d5779f89a6b2c5436816f3bfb32a
Md Ahsan Habib <habibahsan9503@gmail.com>
2026-05-08 16:34:15 +0600

    Implement input and unique value output
    Added functionality to read integers into a vector and print unique values.

--- STL-01.cpp
+++ STL-01.cpp
@@ -216,5 +216,31 @@ Input
 
 Output
 5
+
+=====================================================================================================================
     */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+
+    cout << v[0] << " ";
+
+    for(int i = 1; i < n; i++){
+        if(v[i] != v[i-1]){
+            cout << v[i] << " ";
+        }
+    }
+    cout << endl;
+}
 
